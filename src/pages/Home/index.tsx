21223359a1298a2e4ed5193296c83b3d197150f3
Bruno Santos <brsantos197@gmail.com>
2022-11-27 23:44:02 -0300

    page: Home em andamento

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
-import { HomeContainer } from './styles'
+import {
+  HomeIntro,
+  IntroInfo,
+  IntroItems,
+  IntroSubTitle,
+  IntroTitle,
+} from './styles'
+
+import image from '../../assets/title-image.png'
 
 export const Home: React.FC = () => {
   return (
-    <HomeContainer>
-      <h1>Home</h1>
-    </HomeContainer>
+    <HomeIntro>
+      <IntroInfo>
+        <IntroTitle>
+          Encontre o café perfeito para qualquer hora do dia
+        </IntroTitle>
+        <IntroSubTitle>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </IntroSubTitle>
+
+        <IntroItems></IntroItems>
+      </IntroInfo>
+      <img src={image} alt="Imagem de introdução" />
+    </HomeIntro>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,36 @@
 import styled from 'styled-components'
+import backgroundImage from '../../assets/home-background.png'
 
-export const HomeContainer = styled.div``
+export const HomeIntro = styled.div`
+  width: 100%;
+  height: 544px;
+  display: flex;
+  gap: 3.5rem;
+  background-image: url(${backgroundImage});
+`
+
+export const IntroInfo = styled.div`
+  padding-left: 10rem;
+`
+
+export const IntroTitle = styled.h1`
+  font-family: '${({ theme }) => theme.FONTS.HEADER.FAMILY}', sans-serif;
+  font-size: ${({ theme }) => theme.FONTS.HEADER.SIZES.XL}px;
+  line-height: ${({ theme }) => theme.FONTS.HEADER.LINE_HEIGHT}%;
+  font-weight: 800;
+
+  color: ${({ theme }) => theme.COLORS['BASE-TITLE']};
+`
+
+export const IntroSubTitle = styled.h2`
+  font-family: '${({ theme }) => theme.FONTS.TEXT.FAMILY}', sans-serif;
+  font-size: ${({ theme }) => theme.FONTS.TEXT.SIZES.XL}px;
+  line-height: ${({ theme }) => theme.FONTS.TEXT.LINE_HEIGHT[130]}%;
+  font-weight: 400;
+
+  margin-top: 1rem;
+
+  color: ${({ theme }) => theme.COLORS['BASE-SUBTITLE']};
+`
+
+export const IntroItems = styled.div``
