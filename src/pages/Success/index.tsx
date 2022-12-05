698c0eabc0a88419d4f29fda4c8852a67c27ba43
Bruno Santos <brsantos197@gmail.com>
2022-12-05 00:08:25 -0300

    page: Success finalizada

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,11 +1,61 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 import React from 'react'
+import { useTheme } from 'styled-components'
+import { IconList } from '../../components/IconList'
 
-import { SuccessContainer } from './styles'
+import {
+  OrderCard,
+  Row,
+  SuccessContainer,
+  SuccessHeader,
+  SuccessSubTitle,
+  SuccessTitle,
+} from './styles'
 
 export const Success: React.FC = () => {
+  const theme = useTheme()
   return (
     <SuccessContainer>
-      <h1>Success</h1>
+      <div>
+        <SuccessHeader>
+          <SuccessTitle>Uhu! Pedido confirmado</SuccessTitle>
+          <SuccessSubTitle>
+            Agora é só aguardar que logo o café chegará até você
+          </SuccessSubTitle>
+        </SuccessHeader>
+        <OrderCard>
+          <Row>
+            <IconList bgColor="PURPLE">
+              <MapPin weight="fill" color={theme.COLORS.WHITE} />
+            </IconList>
+            <div>
+              <p>
+                Entrega em <b>Rua Major Santos Silva, 19</b>
+              </p>
+              <p>Embaré - Santos, SP</p>
+            </div>
+          </Row>
+          <Row>
+            <IconList bgColor="YELLOW">
+              <Timer weight="fill" color={theme.COLORS.WHITE} />
+            </IconList>
+            <div>
+              <p>Previsão de entrega</p>
+              <b>Embaré - Santos, SP</b>
+            </div>
+          </Row>
+          <Row>
+            <IconList bgColor="YELLOW-DARK">
+              <CurrencyDollar weight="fill" color={theme.COLORS.WHITE} />
+            </IconList>
+            <div>
+              <p>Pagamento na entrega</p>
+              <b>Cartão de Crédito</b>
+            </div>
+          </Row>
+        </OrderCard>
+      </div>
+      <img src="public/assets/delivery.svg" alt="" />
     </SuccessContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,71 @@
 import styled from 'styled-components'
 
-export const SuccessContainer = styled.div``
+export const SuccessContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  margin-inline: 10rem;
+  margin-top: 5rem;
+`
+export const SuccessHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  margin-bottom: 2.5rem;
+`
+
+export const SuccessTitle = styled.h2`
+  font-size: ${({ theme }) => theme.FONTS.HEADER.SIZES.LG}px;
+  font-family: '${({ theme }) => theme.FONTS.HEADER.FAMILY}', sans-serif;
+  line-height: ${({ theme }) => theme.FONTS.HEADER.LINE_HEIGHT}%;
+  font-weight: 800;
+
+  color: ${({ theme }) => theme.COLORS['YELLOW-DARK']};
+`
+export const SuccessSubTitle = styled.p`
+  font-size: ${({ theme }) => theme.FONTS.TEXT.SIZES.LG}px;
+  line-height: ${({ theme }) => theme.FONTS.TEXT.LINE_HEIGHT[130]}%;
+
+  color: ${({ theme }) => theme.COLORS['BASE-SUBTITLE']};
+`
+export const OrderCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 2.5rem;
+  gap: 2rem;
+
+  border: 1px solid transparent;
+
+  border-radius: 6px 36px;
+  background: white;
+  background-clip: padding-box;
+
+  position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -1px; /* !importanté */
+    border-radius: inherit; /* !importanté */
+    background: linear-gradient(
+      to right,
+      ${({ theme }) => theme.COLORS.YELLOW},
+      ${({ theme }) => theme.COLORS.PURPLE}
+    );
+  }
+`
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  p {
+    line-height: ${({ theme }) => theme.FONTS.TEXT.LINE_HEIGHT[130]}%;
+  }
+`
